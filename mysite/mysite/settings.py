3023e3dfda33c40a631b4c66955551458d8020d2
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2016-12-15 17:27:16 +0200

    'deployment-error'

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -104,6 +104,13 @@ DATABASES = {
     }
 }
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 #import dj_database_url
 #DATABASES['default'] = dj_database_url.config(default='mongodb://work:work@ds131878.mlab.com:31878/work')
 
