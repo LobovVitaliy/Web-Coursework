cc2f685f1129b02681517592702d49dcbccd66b5
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2016-12-15 02:34:23 +0200

    'deployment-sqlite3'

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,12 +77,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+import dj_database_url
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # from mongoengine import connect
 # connect('test')
