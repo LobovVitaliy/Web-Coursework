cf906b5e23eb94b0ad1f6c5eb137e79dfd8f5484
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2016-12-15 02:58:57 +0200

    'deployment-db'

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,8 +77,6 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-import dj_database_url
-
 
 DATABASES = {
     'default': {
@@ -87,15 +85,21 @@ DATABASES = {
     }
 }
 
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
 
 # from mongoengine import connect
 # connect('test')
