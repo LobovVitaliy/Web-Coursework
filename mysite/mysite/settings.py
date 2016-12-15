c988106ddcdc60d2ddebf31a6e3f1c2cd47b85b7
LobovVitaliy <vitalik.lobov.fpm.51@gmail.com>
2016-12-15 19:58:07 +0200

    'deployment-mongodb'

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -107,16 +107,11 @@ DATABASES = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
-try:
-    from .local_settings import *
-except ImportError:
-    pass
 
 #import dj_database_url
 #DATABASES['default'] = dj_database_url.config(default='mongodb://work:work@ds131878.mlab.com:31878/work')
